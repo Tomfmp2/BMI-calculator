7e4a9d135ef0e286d05f27a3530b42b587c16b8e
Tomfmp2 <222234754+Tomfmp2@users.noreply.github.com>
2025-10-16 21:56:32 -0500

    feat :sparkles: Project Completed ....    Some improvements can be made to adapt it to the screens

--- js/app.js
+++ js/app.js
@@ -1,19 +1,49 @@
-const altura = document.getElementById("altura");
-const peso = document.getElementById("peso");
-const imc = peso / (altura**2);
+const alturaInput = document.getElementById("altura");
+const pesoInput = document.getElementById("peso");
+const calcularBtn = document.getElementById("btn-calcular")
+const resultadoP = document.getElementById('resultado');
+const imcImagen = document.getElementById('imc-img')
 
-// Calificacion de IMC
-if (imc <= 18.5){
-	console.log("Tu peso es Inferior al normal")
-} else if (imc > 18.5 && imc < 24.9){
-	console.log("Tu peso es Normal")
-} else if  (imc > 25 && imc <= 29.9){
-	console.log("Estas en Sobrepeso ")
-}else if( imc >= 30 && imc < 34.9){
-	console.log("Tienes Obesidad I")
-}else if (imc >= 35 && imc < 39.9){
-	console.log("Tienes Obesidad II")
-}else if (imc > 40){
-	console.log("Tienes Obesidad III")
-}
-console.log(imc)
+calcularBtn.addEventListener('click', () => {
+    // Obtener los valores de los inputs
+    const altura = parseFloat(alturaInput.value);
+    const peso = parseFloat(pesoInput.value);
+    let mensaje = '';
+    let imagenNivelImc = '';
+
+    // Validar que los valores sean números válidos
+    if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
+        resultadoP.textContent = 'Por favor ingresar numeros validos.';
+	imcImagen.src = '../assets/img/error.png'
+	return;
+    }
+
+    // Realizar la operación (cálculo de IMC)
+    const imc = peso / (altura * altura);
+
+    // Clasificación del IC
+    if (imc <= 18.5) {
+        mensaje = "Tu peso es Inferior al normal.";
+	imcImagen.src = '../assets/img/IMC-Indice-de-Masa-Corporal-<-18-5.webp';
+	
+    } else if (imc > 18.5 && imc < 24.9) {
+        mensaje = "Tu peso es Normal.";
+	imcImagen.src = '../assets/img/IMC-Indice-de-Masa-Corporal-saludable.webp';
+    } else if (imc >= 25 && imc <= 29.9) {
+        mensaje = "Estás en Sobrepeso.";
+	imcImagen.src = '../assets/img/IMC-Indice-de-Masa-Corporal-sobrepeso.webp'
+    } else if (imc >= 30 && imc < 34.9) {
+        mensaje = "Tienes Obesidad I.";
+	imcImagen.src = '../assets/img/IMC-Indice-de-Masa-Corporal-obesidad-I.webp'
+    } else if (imc >= 35 && imc < 39.9) {
+        mensaje = "Tienes Obesidad II.";
+	imcImagen.src = '../assets/img/IMC-Indice-de-Masa-Corporal-obesidad-II.webp'
+    } else if (imc >= 40) {
+        mensaje = "Tienes Obesidad III.";
+	imcImagen.src = '../assets/img/IMC-Indice-de-Masa-Corporal-obesidad-III.webp'
+    }
+
+    // Mostrar el resultado con la clasificación
+    resultadoP.textContent = `Tu IMC es: ${imc.toFixed(1)} ${mensaje}`;
+});
+
